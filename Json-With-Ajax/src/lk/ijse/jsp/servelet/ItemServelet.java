e0a4bc798396ca7b7de98281a1a74126d6d3fa46
Iman Adithya <121780063+ImanAdithya@users.noreply.github.com>
2023-08-19 20:43:06 +0530

    content updated

---
+++ Json-With-Ajax/src/lk/ijse/jsp/servelet/ItemServelet.java
@@ -0,0 +1,17 @@
+package lk.ijse.jsp.servelet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/pages/item"})
+public class ItemServelet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
