54223dc3732ff384eabc8d4e1e7a6bd1b065be89
Iman Adithya <121780063+ImanAdithya@users.noreply.github.com>
2023-08-19 09:41:22 +0530

    content updated

--- Json-With-Ajax/src/lk/ijse/jsp/servelet/CustomerServelet.java
+++ Json-With-Ajax/src/lk/ijse/jsp/servelet/CustomerServelet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(urlPatterns = {"/pages/customer"})
 public class CustomerServelet extends HttpServlet {
 
     @Override
