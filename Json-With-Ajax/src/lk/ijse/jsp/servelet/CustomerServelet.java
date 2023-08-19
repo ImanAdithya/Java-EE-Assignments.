3f1a797a0b5d8dbcf1a978bbbfe1bc29dc5b8047
Iman Adithya <121780063+ImanAdithya@users.noreply.github.com>
2023-08-19 18:51:40 +0530

    content updated

--- Json-With-Ajax/src/lk/ijse/jsp/servelet/CustomerServelet.java
+++ Json-With-Ajax/src/lk/ijse/jsp/servelet/CustomerServelet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
@@ -18,36 +19,36 @@ public class CustomerServelet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/AjaxJson", "root", "12345678");
-            PreparedStatement pstm = connection.prepareStatement("select * from Customer");
-            ResultSet rst = pstm.executeQuery();
-            PrintWriter writer=resp.getWriter ();
-            resp.addHeader("Content-Type","application/json");
+            Class.forName ("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection ("jdbc:mysql://localhost:3306/AjaxJson", "root", "12345678");
+            PreparedStatement pstm = connection.prepareStatement ("select * from Customer");
+            ResultSet rst = pstm.executeQuery ();
+            PrintWriter writer = resp.getWriter ();
+            resp.addHeader ("Content-Type", "application/json");
 
-            JsonArrayBuilder allCustomer= Json.createArrayBuilder ();
+            JsonArrayBuilder allCustomer = Json.createArrayBuilder ();
 
-            while(rst.next ()){
-                String id = rst.getString(1);
-                String name = rst.getString(2);
-                String address = rst.getString(3);
-                String salary = (rst.getString(4));
+            while (rst.next ()) {
+                String id = rst.getString (1);
+                String name = rst.getString (2);
+                String address = rst.getString (3);
+                String salary = (rst.getString (4));
 
-                JsonObjectBuilder customer=Json.createObjectBuilder ();
+                JsonObjectBuilder customer = Json.createObjectBuilder ();
 
-                customer.add("id",id);
-                customer.add("name",name);
-                customer.add("address",address);
-                customer.add("salary",salary);
+                customer.add ("id", id);
+                customer.add ("name", name);
+                customer.add ("address", address);
+                customer.add ("salary", salary);
 
-                allCustomer.add(customer.build());
+                allCustomer.add (customer.build ());
             }
-            writer.print(allCustomer.build());
+            writer.print (allCustomer.build ());
 
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException (e);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException (e);
         }
 
     }
@@ -63,7 +64,7 @@ public class CustomerServelet extends HttpServlet {
         try {
             Class.forName ("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection ("jdbc:mysql://localhost:3306/AjaxJson", "root", "12345678");
-            PreparedStatement pstm = connection.prepareStatement("insert into customer values(?,?,?,?)");
+            PreparedStatement pstm = connection.prepareStatement ("insert into customer values(?,?,?,?)");
 
             pstm.setObject (1, cusId);
             pstm.setObject (2, cusName);
@@ -76,28 +77,51 @@ public class CustomerServelet extends HttpServlet {
             throw new RuntimeException (e);
         }
 
-        }
+    }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String cusId = req.getParameter ("cID");
-        String cusName = req.getParameter ("cName");
-        String cusAddress = req.getParameter ("cAddress");
-        String salary = req.getParameter ("cSalary");
+
+        String cusId = req.getParameter ("id");
+        String cusName = req.getParameter ("name");
+        String cusAddress = req.getParameter ("address");
+
 
         try {
-        Class.forName ("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection ("jdbc:mysql://localhost:3306/AjaxJson", "root", "12345678");
-        PreparedStatement pstm3 = connection.prepareStatement("update Customer set name=?,address=? where id=?");
-        pstm3.setObject(3, cusId);
-        pstm3.setObject(1, cusName);
-        pstm3.setObject(2, cusAddress);
-        if (pstm3.executeUpdate() > 0) {
-            resp.getWriter().println("Customer Updated..!");
-        }
+
+            Class.forName ("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection ("jdbc:mysql://localhost:3306/AjaxJson", "root", "12345678");
+            PreparedStatement pstm3 = connection.prepareStatement ("update Customer set name=?,address=? where id=?");
+            pstm3.setObject (3, cusId);
+            pstm3.setObject (1, cusName);
+            pstm3.setObject (2, cusAddress);
+            if (pstm3.executeUpdate () > 0) {
+                System.out.println ("Customer Updated");
+            } else {
+                throw new SQLException ();
+            }
+
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException (e);
         }
 
     }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String cusID=req.getParameter ("id");
+        try {
+            Class.forName ("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection ("jdbc:mysql://localhost:3306/AjaxJson", "root", "12345678");
+            PreparedStatement pstm2 = connection.prepareStatement ("delete from Customer where id=?");
+            pstm2.setObject (1, cusID);
+            if (pstm2.executeUpdate () > 0) {
+                resp.getWriter ().println ("Customer Deleted..!");
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException (e);
+        }
+    }
+
 }
